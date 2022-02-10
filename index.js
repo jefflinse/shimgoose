d526187efc5d0659f94a01b7251d263674671634
Jeff Linse <s-jeff.linse@subsplash.com>
2022-02-10 12:54:30 -0800

    Code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Some "external" data residing in a 3rd party API
 const externalTacoData = {
   "62056e13d30a1cb15f585ce4": [{ mongo_id: "62056e13d30a1cb15f585ce4", protein: "fish", spicy: false }],
   "62056e13d30a1cb15f585ce5": [{ mongo_id: "62056e13d30a1cb15f585ce5", protein: "chorizo", spicy: true }],
@@ -13,10 +14,10 @@ async function fetchTacosFromExternalAPI(mongo_id) {
   return externalTacoData[mongo_id]
 }
 
-// store original Query.exec function
+// store original Query.exec function so we can forward any non-intercepted to MongoDB
 const exec = mongoose.Query.prototype.exec
 
-// redefine Query.exec function to fetch data from our API in some cases,
+// redefine the Query.exec function to fetch data from our API in some cases,
 // and to call the original exec function otherwise
 mongoose.Query.prototype.exec = async function(a,b) {
   
@@ -36,14 +37,26 @@ mongoose.Query.prototype.exec = async function(a,b) {
       console.log('error fetching tacos from external API', e);
     }
     
-    return tacos
+    return tacos[0] || null;
   }
 
-  console.log('calling original exec()');
+  // non-intercepted queries will be forwarded to MongoDB
   return exec.apply(this, arguments);
 }
 
+// Populate MongoDB with some sample data
+async function populateData(t) {
+    // Add some test data
+    await t.deleteMany()
+    await new t({ protein: 'beef', spicy: false }).save()
+    await new t({ protein: 'chicken', spicy: true }).save()
+}
+
 main().catch(err => console.log(err));
+
+
+
+// --- MAIN ---------------------------------------------------------------------
 
 async function main() {
   // connect to MongoDB
@@ -57,30 +70,18 @@ async function main() {
     spicy: false,
   });
 
-  tacoSchema.methods.style = function () {
-    return this._id + ': ' + (this.spicy ? 'spicy' : 'plain') + ' ' + this.protein + ' taco';
-  };
 
+  // create the model
   const Taco = mongoose.model('Taco', tacoSchema);
 
-  const [plainBeef, spicyChicken] = await populateData(Taco);
+  // populate some test data in MongoDB
+  await populateData(Taco);
 
+  let tacos = await Taco.find()
+  console.log('Tacos in MongoDB:', tacos);
+
+  // this findOne call will be intercepted and will return data from our API instead
   console.log('attempting to find a taco that only exists in the external API');
   let taco = await Taco.findOne({ _id: "62056e13d30a1cb15f585ce5" /* chorizo (external data) */ });
-  console.log(taco);
+  console.log('Taco found:', taco);
 }
-
-async function populateData(t) {
-    // Add some test data
-    await t.deleteMany()
-
-    const plainBeef = new t({ protein: 'beef', spicy: false });
-    await plainBeef.save();
-  
-    const spicyChicken = new t({ protein: 'chicken', spicy: true });
-    await spicyChicken.save();
-
-    return [plainBeef, spicyChicken];
-}
-
-
