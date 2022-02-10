0410096e6fd45adcdfc10ffb1a680762923a1953
Jeff Linse <s-jeff.linse@subsplash.com>
2022-02-10 12:21:38 -0800

    Fix ID replacement

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 const mongoose = require('mongoose');
 
+const externalTacoData = {
+  "62056e13d30a1cb15f585ce4": [{ mongo_id: "62056e13d30a1cb15f585ce4", protein: "fish", spicy: false }],
+  "62056e13d30a1cb15f585ce5": [{ mongo_id: "62056e13d30a1cb15f585ce5", protein: "chorizo", spicy: true }],
+  "62056e13d30a1cb15f585ce6": [{ mongo_id: "62056e13d30a1cb15f585ce6", protein: "carne asada", spicy: false }],
+  "62056e13d30a1cb15f585ce7": [{ mongo_id: "62056e13d30a1cb15f585ce7", protein: "tofu", spicy: false }],
+  "62056e13d30a1cb15f585ce8": [{ mongo_id: "62056e13d30a1cb15f585ce8", protein: "black bean", spicy: true }],
+};
+
+// Simulate an external API call that returns tacos.
+async function fetchTacosFromExternalAPI(mongo_id) {
+  return externalTacoData[mongo_id]
+}
+
 // store original Query.exec function
 const exec = mongoose.Query.prototype.exec
 
-// reassign Query.exec function to fetch data from our API instead
-mongoose.Query.prototype.exec = function(a,b) {
-  console.log('intercepting query execution');
-  console.log(this)
-
+// redefine Query.exec function to fetch data from our API in some cases,
+// and to call the original exec function otherwise
+mongoose.Query.prototype.exec = async function(a,b) {
+  
   if (this.op === 'findOne') {
-    const content = {
-      protein: 'black bean',
-      spicy: true,
-    };
-
-    return Array.isArray(content)
-          ? Promise.resolve(content.map(doc => new this.model(doc)))
-          : Promise.resolve(new this.model(content))
+    console.log(`intercepting '${this.op}' query execution, fetching from external API instead`);
+    let tacos = [];
+    try {
+      tacos = await fetchTacosFromExternalAPI(this.getQuery()._id)
+      if (tacos.length > 0) {
+        tacos = tacos.map(taco => {
+          taco._id = taco.mongo_id
+          return taco
+        });
+        tacos = tacos.map(taco => new this.model(taco))
+      }
+    } catch (e) {
+      console.log('error fetching tacos from external API', e);
+    }
+    
+    return tacos
   }
 
+  console.log('calling original exec()');
   return exec.apply(this, arguments);
 }
 
 main().catch(err => console.log(err));
 
 async function main() {
-  console.log('Connecting to database...');
+  // connect to MongoDB
   await mongoose.connect('mongodb://root:example@localhost:27017/test', {
     authSource: 'admin'
   });
   
+  // create the schema
   const tacoSchema = new mongoose.Schema({
     protein: String,
     spicy: false,
@@ -41,23 +63,10 @@ async function main() {
 
   const Taco = mongoose.model('Taco', tacoSchema);
 
-  const [plainVeggie, spicyChicken] = await populateData(Taco);
+  const [plainBeef, spicyChicken] = await populateData(Taco);
 
-  // Taco.findById = function (id, projection, options) {
-  //   return Taco.findOne({ _id: id });
-  // };
-
-  // Taco.findOne = function(filter, projection, options) {
-  //   console.log('findOne() ' + filter._id);
-  //   return new Taco({
-  //     _id: filter._id,
-  //     protein: 'black bean',
-  //     spicy: true,
-  //   })
-  // }
-
-  console.log('calling Taco.findOne()');
-  let taco = await Taco.findOne({ _id: spicyChicken._id });
+  console.log('attempting to find a taco that only exists in the external API');
+  let taco = await Taco.findOne({ _id: "62056e13d30a1cb15f585ce5" /* chorizo (external data) */ });
   console.log(taco);
 }
 
@@ -65,13 +74,13 @@ async function populateData(t) {
     // Add some test data
     await t.deleteMany()
 
-    const plainVeggie = new t({ protein: 'veggie', spicy: false });
-    await plainVeggie.save();
-    console.log(plainVeggie.style());
+    const plainBeef = new t({ protein: 'beef', spicy: false });
+    await plainBeef.save();
   
     const spicyChicken = new t({ protein: 'chicken', spicy: true });
     await spicyChicken.save();
-    console.log(spicyChicken.style());
 
-    return [plainVeggie, spicyChicken];
+    return [plainBeef, spicyChicken];
 }
+
+
