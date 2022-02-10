3cdc872b6f8e257d3ac55c63f277a44b60960712
Jeff Linse <s-jeff.linse@subsplash.com>
2022-02-09 20:04:41 -0800

    Add some example code from the mongoose Getting Started samples

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  console.log('Connecting to database...');
+  await mongoose.connect('mongodb://root:example@localhost:27017/test', {
+    authSource: 'admin'
+  });
+  
+  const tacoSchema = new mongoose.Schema({
+    protein: String,
+    spicy: false,
+  });
+
+  tacoSchema.methods.style = function () {
+    return (this.spicy ? 'spicy' : 'plain') + ' ' + this.protein + ' taco';
+  };
+
+  const Taco = mongoose.model('Taco', tacoSchema);
+
+  const plainVeggie = new Taco({ protein: 'veggie', spicy: false });
+  console.log(plainVeggie.style());
+  const spicyChicken = new Taco({ protein: 'chicken', spicy: true });
+  console.log(spicyChicken.style());
+
+  await plainVeggie.save();
+  await spicyChicken.save();
+  
+  let tacos = await Taco.find();
+  console.log(tacos);
+
+  tacos = await Taco.find({ protein: /^chi/ });
+  console.log(tacos);
+}
