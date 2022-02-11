b8d6d4f695ebd04a2c00b1f4cbf1b36fd326d2f3
Jeff Linse <s-jeff.linse@subsplash.com>
2022-02-11 12:53:07 -0800

    Sort of working but still makes a MongoDB call

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const applyHooks  = require('mongoose/lib/helpers/model/applyHooks');
+const promiseOrCallback  = require('mongoose/lib/helpers/promiseOrCallback');
 
 // Some "external" data residing in a 3rd party API
 const externalTacoData = {
@@ -14,35 +16,102 @@ async function fetchTacosFromExternalAPI(mongo_id) {
   return externalTacoData[mongo_id]
 }
 
-// store the original Query.exec function so we can forward any non-intercepted calls to MongoDB
-const exec = mongoose.Query.prototype.exec
-
 // redefine the Query.exec function to fetch data from our API in some cases,
-// and to call the original exec function otherwise
-mongoose.Query.prototype.exec = async function() {
+// and to execute the original query otherwise
+
+mongoose.Query.prototype.exec = function exec(op, callback) {
   console.log(`Query.exec(${this.op}) called`);
-  
-  if (this.op === 'findOne') {
-    console.log(`intercepting '${this.op}' query execution, fetching from external API instead`);
-    let tacos = [];
-    try {
-      tacos = await fetchTacosFromExternalAPI(this.getQuery()._id)
-      if (tacos.length > 0) {
-        tacos = tacos.map(taco => {
-          taco._id = taco.mongo_id
-          return taco
-        });
-        tacos = tacos.map(taco => new this.model(taco))
-      }
-    } catch (e) {
-      console.log('error fetching tacos from external API', e);
-    }
-    
-    return tacos[0] || null;
+
+  const _this = this;
+  // Ensure that `exec()` is the first thing that shows up in
+  // the stack when cast errors happen.
+  const castError = new mongoose.Error.CastError();
+
+  if (typeof op === 'function') {
+    callback = op;
+    op = null;
+  } else if (typeof op === 'string') {
+    this.op = op;
   }
 
-  // non-intercepted queries will be forwarded to MongoDB
-  return exec.apply(this, arguments);
+  if (this.op == null) {
+    throw new Error('Query must have `op` before executing');
+  }
+  this._validateOp();
+
+  callback = this.model.$handleCallbackError(callback);
+
+  return promiseOrCallback(callback, (cb) => {
+    cb = this.model.$wrapCallback(cb);
+
+    if (!_this.op) {
+      cb();
+      return;
+    }
+
+    this._hooks.execPre('exec', this, [], (error) => {
+      if (error != null) {
+        return cb(_cleanCastErrorStack(castError, error));
+      }
+      let thunk = '_' + this.op;
+      if (this.op === 'update') {
+        thunk = '_execUpdate';
+      } else if (this.op === 'distinct') {
+        thunk = '__distinct';
+      }
+
+      // intercept 'findOne' queries and return data from our API instead
+      if (this.op === 'findOne') {
+        console.log(`intercepting '${this.op}' query execution, fetching from external API instead`);
+        let tacos = [];
+
+        // DIRTY WAY
+        // execute the underlying MongoDB thunk and discard the result
+        this[thunk].call(this, (error, res) => {})
+        // DIRTY WAY
+
+        fetchTacosFromExternalAPI(this.getQuery()._id)
+          .then(tacos => {
+            if (tacos.length > 0) {
+              tacos = tacos.map(taco => {
+                taco._id = taco.mongo_id;
+                return taco;
+              });
+              tacos = tacos.map(taco => new this.model(taco));
+            }
+            cb(null, tacos[0] || null);
+          })
+          .catch(err => {
+            console.log('error fetching tacos from external API', e);
+            return cb(_cleanCastErrorStack(castError, err));
+          })
+      } else {
+        // execute the MongoDB query
+        this[thunk].call(this, (error, res) => {
+          if (error) {
+            return cb(_cleanCastErrorStack(castError, error));
+          }
+
+          this._hooks.execPost('exec', this, [], {}, (error) => {
+            if (error) {
+              return cb(_cleanCastErrorStack(castError, error));
+            }
+
+            cb(null, res);
+          });
+        });
+      }
+    });
+  }, this.model.events);
+};
+
+function _cleanCastErrorStack(castError, error) {
+  if (error instanceof mongoose.Error.CastError) {
+    castError.copy(error);
+    return castError;
+  }
+
+  return error;
 }
 
 // Populate MongoDB with some sample data
@@ -72,12 +141,11 @@ async function main() {
   });
 
   // pre/post hooks should continue to work as expected
-  tacoSchema.pre('findOne', function(next) {
-    console.log('pre findOne');
-    next();
+  tacoSchema.pre('findOne', function() {
+    console.log('PRE findOne');
   });
   tacoSchema.post('findOne', function() {
-    console.log('post findOne');
+    console.log('POST findOne');
   });
 
 
