0eaf7aea8e04c3059e28ab099e8f13a274fdfa0f
Jeff Linse <s-jeff.linse@subsplash.com>
2022-02-11 16:35:51 -0800

    Execute pre/post hooks

--- index.js
+++ index.js
@@ -15,26 +15,35 @@ async function main() {
     spicy: false,
   });
 
-  // these pre/post hooks should continue to work as expected
+  // these pre/post hooks should continue to work as expected, with caveats
   tacoSchema.pre('findOne', function(next) {
-    console.log('pre findOne');
+    console.log('pre findOne 1');
     next();
   });
-  tacoSchema.post('findOne', function() {
-    console.log('post findOne');
+  tacoSchema.pre('findOne', function(next) {
+    console.log('pre findOne 2');
+    next();
+  });
+  tacoSchema.post('findOne', function(next) {
+    console.log('post findOne 1');
+    next()
+  });
+  tacoSchema.post('findOne', function(next) {
+    console.log('post findOne 2');
+    next()
   });
 
-  // create the model using the shim, passing the original Mongoose schema
-  const Taco = new shimgoose.Model('Taco', tacoSchema._schema);
+  // create the shim model using the shim schema
+  const Taco = new shimgoose.Model('Taco', tacoSchema);
 
   // populate some test data in MongoDB
-  // the shim methods can be bypassed by using the original underlying Mongoose model
-  await populateData(Taco._model);
+  // the shim methods can be bypassed by using _mg, the original underlying Mongoose model
+  await Taco._mg.deleteMany()
+	await new Taco._mg({ protein: 'beef', spicy: false }).save()
+	await new Taco._mg({ protein: 'chicken', spicy: true }).save()
+  console.log('Tacos in MongoDB:', await Taco._mg.find());
 
-  let tacos = await Taco._model.find()
-  console.log('Tacos in MongoDB:', tacos);
-
-  // this findOne call will be intercepted and will return data from our API instead
+  // this findOne call will bypass Mongoose entirely and fetch from our API instead
   console.log('attempting to find a taco that only exists in the external API');
   try {
     let taco = await Taco.findOne({ _id: "62056e13d30a1cb15f585ce5" /* chorizo (external data) */ });
@@ -48,7 +57,5 @@ async function main() {
 // Populate MongoDB with some sample data
 async function populateData(t) {
 	// Add some test data
-	await t.deleteMany()
-	await new t({ protein: 'beef', spicy: false }).save()
-	await new t({ protein: 'chicken', spicy: true }).save()
+	
 }
--- shimgoose.js
+++ shimgoose.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const externalapi  = require('./externalapi');
 
+// Schema wraps the Mongoose Schema type and keeps track of pre/post hooks manually.
 function Schema(definition) {
-  this._schema = new mongoose.Schema(definition);
-    this.hooks = {
-      pre: {},
-      post: {}
+  this._mg = new mongoose.Schema(definition);
+  this.hooks = {
+    pre: {},
+    post: {}
   }
 }
 
@@ -23,29 +24,51 @@ if (this.hooks.pre[event] === undefined) {
 // event: string ('findOne', 'save', etc.)
 // callback: function(next)
 Schema.prototype.post = function(event, callback) {
-  if (this.hooks.pre[event] === undefined) {
-    this.hooks.pre[event] = [];
+  if (this.hooks.post[event] === undefined) {
+    this.hooks.post[event] = [];
   }
-  this.hooks.pre[event].push(callback);
+  this.hooks.post[event].push(callback);
 }
 
+// Model wraps the Mongoose Model type and keeps a reference to its Schema.
 function Model(name, schema) {
-  this._model = mongoose.model(name, schema);
+  this._schema = schema
+  this._mg = mongoose.model(name, this._schema._mg);
 }
 
 Model.prototype.findOne = async function(filter) {
+
+  // run pre-hooks
+  runHooks(this, 'pre', 'findOne');
+
+  // fetch data from external API
+  console.log('invoking external API')
   let data = await externalapi.fetchTacos(filter._id)
   if (data.length === 0) {
     return null;
   }
 
+  // assign ID and create model
   let taco = data[0];
   taco._id = taco.mongo_id
-  return await this._model.create(data);
+  let t = await this._mg.create(data);
+
+  // run post-hooks
+  runHooks(this, 'post', 'findOne');
+
+  return t;
 }
 
-Model.prototype.deleteMany = async function() {
-  this._model.deleteMany(arguments)
+// Runs hooks synchronously and sequentially. Probably not what we want.
+function runHooks(model, phase, event) {
+  const hooks = model._schema.hooks[phase][event];
+  if (hooks) {
+    hooks.forEach((hook, i) => {
+      hook.call(model._mg, () => {
+        console.log(`invoked ${model._mg.modelName} ${phase} ${event} hook #${i}`);
+      });
+    });
+  }
 }
 
 module.exports = {
